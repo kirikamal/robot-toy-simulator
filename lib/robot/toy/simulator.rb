e3ddfa2c24897bea3ca3ff766652f49cab0aec93
Kiri Kamal <kirikamal@gmail.com>
2017-08-27 08:47:10 +1000

    Initial commit

---
+++ lib/robot/toy/simulator.rb
@@ -0,0 +1,9 @@
+require "robot/toy/simulator/version"
+
+module Robot
+  module Toy
+    module Simulator
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/robot/toy/simulator/version.rb
@@ -0,0 +1,7 @@
+module Robot
+  module Toy
+    module Simulator
+      VERSION = "0.1.0"
+    end
+  end
+end
